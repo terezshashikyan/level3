b61aa7fe925f7269c8a9ed032f97df8e1716804e
bella-shell <bella.shelllogix@gmail.com>
2023-11-10 14:27:02 +0400

    changes

--- src/pages/Auction/UpcomingLots/index.tsx
+++ src/pages/Auction/UpcomingLots/index.tsx
@@ -1,36 +1,40 @@
 import { useState } from "react";
 import { LOTS } from "./constants";
 
-import './style.scss';
+import "./style.scss";
 
 const UpcomingLots = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleOpen = () => setIsOpen(!isOpen);
+  const toggleOpen = () => setIsOpen(!isOpen);
 
-    const lotsRenderer = LOTS.map((lot) => (
-     <div className="upcomingLots__lot">
-       <img className="upcomingLots__lot_img" src={lot.src} alt="lot" />
-       <div className="upcomingLots__lot_details">
+  const lotsRenderer = LOTS.map((lot) => (
+    <div className="upcomingLots__lot">
+      <div className="upcomingLots__lot_img">
+      <img src={lot.src} alt="lot" />
+      <button className="upcomingLots__lot_img_btn">Next</button>
+      </div>
+      <div className="upcomingLots__lot_details">
         <span className="upcomingLots__lot_details_name">{lot.name}</span>
         <span className="upcomingLots__lot_details_text">Lot: {lot.lot}</span>
         <span className="upcomingLots__lot_details_text">Lot: {lot.lot}</span>
-       </div>
-     </div>
-    ))
-   return (
-     <section className = 'upcomingLots'>
-        <div className="upComingLots__desktop">
-        {lotsRenderer}
-        </div>
-        <div className="upComingLots__mobileAndTablet">
-        <button className="upcomingLots__mobileAndTablet__button" onClick={toggleOpen}>
-            Upcoming Lots
+      </div>
+    </div>
+  ));
+  return (
+    <section className="upcomingLots">
+      <div className="upcomingLots__desktop">{lotsRenderer}</div>
+      <div className="upcomingLots__mobileAndTablet">
+        <button
+          className="upcomingLots__mobileAndTablet__button"
+          onClick={toggleOpen}
+        >
+          Upcoming Lots
         </button>
-        {isOpen?lotsRenderer:null}
-        </div>
-     </section>
-   )
-}
+        {isOpen ? lotsRenderer : null}
+      </div>
+    </section>
+  );
+};
 
 export default UpcomingLots;
